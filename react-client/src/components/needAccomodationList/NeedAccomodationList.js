79b1034d06368393f5ecd58c9e2d904205c78099
Krisztian Ivan <krisztian.ivan@gmail.com>
2023-11-23 11:32:54 +0000

    added pagination

--- react-client/src/components/needAccomodationList/NeedAccomodationList.js
+++ react-client/src/components/needAccomodationList/NeedAccomodationList.js
@@ -41,6 +41,34 @@ function NeedAccomodationList() {
         </button>
       </div>
 
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center mt-3">
+          <li class="page-item disabled">
+            <a class="page-link">Previous</a>
+          </li>
+          <li class="page-item">
+            <a class="page-item active page-link" href="#">
+              1
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              2
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              3
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
+
       <AccomodationFinderModal />
     </div>
   );
