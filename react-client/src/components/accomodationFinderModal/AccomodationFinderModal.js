f90d928504d26db07f7e435e24fc757bd56689e1
Krisztian Ivan <krisztian.ivan@gmail.com>
2023-11-23 11:16:05 +0000

    added accomodation finder modal

---
+++ react-client/src/components/accomodationFinderModal/AccomodationFinderModal.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function AccomodationFinderModal() {
+  return (
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">
+              I will launch a search for accomodation
+            </h1>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">...and will show the results here.</div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Cancel
+            </button>
+            <button type="button" class="btn btn-primary">
+              Confirm Accomodation
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AccomodationFinderModal;
--- react-client/src/components/needAccomodationList/NeedAccomodationList.js
+++ react-client/src/components/needAccomodationList/NeedAccomodationList.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import AccomodationFinderModal from "../accomodationFinderModal/AccomodationFinderModal";
+
 function NeedAccomodationList() {
   return (
     <div class="container mt-4">
@@ -8,19 +10,38 @@ function NeedAccomodationList() {
           type="button"
           class="list-group-item list-group-item-action active"
           aria-current="true"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
         >
           I'm the profile of a homeless person. Click me to find accomodation.
         </button>
-        <button type="button" class="list-group-item list-group-item-action">
+        <button
+          type="button"
+          class="list-group-item list-group-item-action"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
           I'm the profile of a homeless person. Click me to find accomodation.
         </button>
-        <button type="button" class="list-group-item list-group-item-action">
+        <button
+          type="button"
+          class="list-group-item list-group-item-action"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
           I'm the profile of a homeless person. Click me to find accomodation.
         </button>
-        <button type="button" class="list-group-item list-group-item-action">
+        <button
+          type="button"
+          class="list-group-item list-group-item-action"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
           I'm the profile of a homeless person. Click me to find accomodation.
         </button>
       </div>
+
+      <AccomodationFinderModal />
     </div>
   );
 }
