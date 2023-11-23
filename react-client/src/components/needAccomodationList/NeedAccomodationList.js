df61ca9ee96b44580a3047ea684302aca99d2f4b
Krisztian Ivan <krisztian.ivan@gmail.com>
2023-11-23 15:00:11 +0000

    changed styling of list

--- react-client/src/components/needAccomodationList/NeedAccomodationList.js
+++ react-client/src/components/needAccomodationList/NeedAccomodationList.js
@@ -4,43 +4,46 @@ import AccomodationFinderModal from "../accomodationFinderModal/AccomodationFind
 
 function NeedAccomodationList() {
   return (
-    <div class="container mt-4">
-      <div class="list-group">
-        <button
-          type="button"
-          class="list-group-item list-group-item-action active"
-          aria-current="true"
-          data-bs-toggle="modal"
-          data-bs-target="#exampleModal"
-        >
-          I'm the profile of a homeless person. Click me to find accomodation.
-        </button>
-        <button
-          type="button"
-          class="list-group-item list-group-item-action"
-          data-bs-toggle="modal"
-          data-bs-target="#exampleModal"
-        >
-          I'm the profile of a homeless person. Click me to find accomodation.
-        </button>
-        <button
-          type="button"
-          class="list-group-item list-group-item-action"
-          data-bs-toggle="modal"
-          data-bs-target="#exampleModal"
-        >
-          I'm the profile of a homeless person. Click me to find accomodation.
-        </button>
-        <button
-          type="button"
-          class="list-group-item list-group-item-action"
-          data-bs-toggle="modal"
-          data-bs-target="#exampleModal"
-        >
-          I'm the profile of a homeless person. Click me to find accomodation.
-        </button>
-      </div>
+    <>
+      <div class="container d-flex justify-content-center mt-4">
+        <div class="list-group max-width-80">
+          <button
+            type="button"
+            class="list-group-item list-group-item-action active"
+            aria-current="true"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            I'm the profile of a homeless person. Click me to find accomodation.
+          </button>
+          <button
+            type="button"
+            class="list-group-item list-group-item-action"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            I'm the profile of a homeless person. Click me to find accomodation.
+          </button>
+          <button
+            type="button"
+            class="list-group-item list-group-item-action"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            I'm the profile of a homeless person. Click me to find accomodation.
+          </button>
+          <button
+            type="button"
+            class="list-group-item list-group-item-action"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            I'm the profile of a homeless person. Click me to find accomodation.
+          </button>
+        </div>
 
+        <AccomodationFinderModal />
+      </div>
       <nav aria-label="Page navigation example">
         <ul class="pagination justify-content-center mt-3">
           <li class="page-item disabled">
@@ -68,9 +71,7 @@ function NeedAccomodationList() {
           </li>
         </ul>
       </nav>
-
-      <AccomodationFinderModal />
-    </div>
+    </>
   );
 }
 
