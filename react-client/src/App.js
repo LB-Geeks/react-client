10ca2495d3df0a2afb7b116e7854a17f3e49627d
Krisztian Ivan <krisztian.ivan@gmail.com>
2023-11-23 09:21:39 +0000

    added boilerplate boxes for statistics

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 
 import Navbar from "./components/navbar/Navbar";
+import StatBox from "./components/statBox/StatBox";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <main>
+        <StatBox />
+      </main>
     </div>
   );
 }
