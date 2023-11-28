10131a239c5829b3232f1f485fcd755a45b5d28f
Krisztian Ivan <krisztian.ivan@gmail.com>
2023-11-28 12:23:55 +0000

    call to backend to get confirmed bookings

--- react-client/src/components/confirmedAccomodationList/ConfirmedAccomodationList.js
+++ react-client/src/components/confirmedAccomodationList/ConfirmedAccomodationList.js
@@ -3,25 +3,59 @@ import axios from "axios";
 
 import ConfirmedAccomodationModal from "../confirmedAccomodationModal/ConfirmedAccomodationModal";
 
-function ConfirmedAccomodationList() {
-  const getAllAccomodation = () => {
-    axios
-      .get(
-        "http://localhost:3100/api/allbookings?api-key=DriPObREdrocIDRehublyupUbiQAkeGEprlsWudU"
-      )
-      .then((response) => {
-        console.log(response.data);
-      });
-  };
+let accomodationObject = {};
+const getAllAccomodation = async () => {
+  await axios
+    .get(
+      "http://localhost:3100/api/allbookings?api-key=DriPObREdrocIDRehublyupUbiQAkeGEprlsWudU"
+    )
+    .then((response) => {
+      accomodationObject = response.data;
+    });
+};
 
+await getAllAccomodation();
+
+function ConfirmedAccomodationList() {
   return (
     <>
-      {getAllAccomodation()}
       <div class="container col mt-4">
-        <div class="list-group">
+        <div class="list-group accordion" id="accordionExample">
           <li class="list-group-item list-group-item-action bg-grey">
             <h5>List of confirmed accomodation</h5>
           </li>
+          <div class="accordion-item">
+            <button
+              type="button"
+              class="list-group-item list-group-item-action d-flex justify-content-start accordion-button collapsed"
+              aria-current="true"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >
+              <h6>
+                {accomodationObject[0].host_id} {accomodationObject[0].guest_id}
+              </h6>
+            </button>
+            <div
+              id="collapseTwo"
+              class="accordion-collapse collapse"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body">
+                <strong>This is the second item's accordion body.</strong> It is
+                hidden by default, until the collapse plugin adds the
+                appropriate classes that we use to style each element. These
+                classes control the overall appearance, as well as the showing
+                and hiding via CSS transitions. You can modify any of this with
+                custom CSS or overriding our default variables. It's also worth
+                noting that just about any HTML can go within the{" "}
+                <code>.accordion-body</code>, though the transition does limit
+                overflow.
+              </div>
+            </div>
+          </div>
           <button
             type="button"
             class="list-group-item list-group-item-action"
